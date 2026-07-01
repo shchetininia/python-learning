2a28faefb8ba82a3ebf112055c6088952950ca36
ShchetininIgor <wowerogi@gmail.com>
2026-07-02 01:46:03 +0300

    Добавлен layer в  neuron.py

--- neuron.py
+++ neuron.py
@@ -6,6 +6,10 @@ def neuron(x, w, b):
     z = w @ x + b
     return sigmoid(z)
 
+def layer(x, W, b):
+        z = W @ x + b
+        return sigmoid(z)
+
 x = np.array([30, 100, -0.5])
 w = np.array([0.8, 1.5, 30])
 b = 5
@@ -15,3 +19,10 @@ w[2] = 400
 print(neuron(x, w, b))
 print(w)
 print(w@x)
+w[2] = 30
+
+W = np.array([w,w-1.22,-w*1.5])
+print(W)
+B = np.array([5,10,15])
+print(layer(x,W,B))
+print(np.shape(x), np.shape(W))
