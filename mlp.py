c06dd3f63f2aeb16a5961a73ee95e26486db21dd
ShchetininIgor <wowerogi@gmail.com>
2026-07-04 17:57:20 +0300

    Relu in mlp.py

--- mlp.py
+++ mlp.py
@@ -1,12 +1,15 @@
 import torch
 import torch.nn as nn
-
 class MyNeuron(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear = nn.Linear(1, 1)
-    def forward(self, x):
-        return torch.sigmoid(self.linear(x))
+        self.hidden = nn.Linear(1, 4)
+        self.output = nn.Linear(4, 1)
+    def forward(self,x):
+        x = torch.relu(self.hidden(x))
+        return torch.sigmoid(self.output(x))
+    
+
 
 model = MyNeuron()
 X = torch.tensor([10, 50, 90, 150], dtype=torch.float32)/150.0
@@ -18,6 +21,7 @@ loss_fn = nn.BCELoss()
 count = 10000
 
 while count > 0:
+    torch.manual_seed(42)
     optimizer.zero_grad()
     y_pred = model(X)
     # L = torch.mean((y_pred - Y)**2)
